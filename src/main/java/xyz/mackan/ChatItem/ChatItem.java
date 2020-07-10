d65a35c2001bb1c905dd76282986ed5e4f7f0e49
Sven65 <thormax10@gmail.com>
2020-07-10 08:01:07 +0200

    Add /ci command

--- src/main/java/xyz/mackan/ChatItem/ChatItem.java
+++ src/main/java/xyz/mackan/ChatItem/ChatItem.java
@@ -5,6 +5,8 @@ package xyz.mackan.ChatItem;
 
 import me.pikamug.localelib.LocaleLib;
 import me.pikamug.localelib.LocaleManager;
+import org.bukkit.plugin.PluginDescriptionFile;
+import xyz.mackan.ChatItem.commands.ChatItemCommand;
 import xyz.mackan.ChatItem.events.PlayerChatEventListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,10 +14,14 @@ public class ChatItem extends JavaPlugin {
 	private static LocaleManager localeManager;
 	private static boolean isEssChatEnabled;
 
+	private static PluginDescriptionFile descriptionFile;
+
 	public static LocaleManager getLocaleManager () {
 		return localeManager;
 	}
 	public static boolean getIsEssChatEnabled () { return isEssChatEnabled; }
+
+	public static PluginDescriptionFile getDescriptionFile () { return descriptionFile; }
 
 	@Override
 	public void onEnable() {
@@ -28,6 +34,10 @@ public class ChatItem extends JavaPlugin {
 		isEssChatEnabled = getServer().getPluginManager().getPlugin("EssentialsChat") != null;
 
 		getLogger().info("[ChatItem] Is EssentialsChat enabled: "+isEssChatEnabled);
+
+		this.getCommand("ci").setExecutor(new ChatItemCommand());
+
+		descriptionFile = this.getDescription();
 	}
 
 	@Override
---
+++ src/main/java/xyz/mackan/ChatItem/commands/ChatItemCommand.java
@@ -0,0 +1,18 @@
+package xyz.mackan.ChatItem.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import xyz.mackan.ChatItem.ChatItem;
+
+public class ChatItemCommand implements CommandExecutor {
+	@Override
+	public boolean onCommand (CommandSender sender, Command command, String label, String[] args) {
+		String version = ChatItem.getDescriptionFile().getVersion();
+		boolean isEssChatEnabled = ChatItem.getIsEssChatEnabled();
+
+		sender.sendMessage("[ChatItems] Version "+version+"\nIs ess chat enabled: "+isEssChatEnabled);
+
+		return true;
+	}
+}
