64cdea6d7927a3ecd2c500c44ab47dd0fbed49a2
Sven65 <thormax10@gmail.com>
2020-07-29 12:31:39 +0200

    Update for Javadocs

--- modules/Plugin/src/main/java/xyz/mackan/ChatItems/types/ChatPattern.java
+++ modules/Plugin/src/main/java/xyz/mackan/ChatItems/types/ChatPattern.java
@@ -1,5 +1,8 @@
 package xyz.mackan.ChatItems.types;
 
+/**
+ * Container class for a pattern to resolve
+ */
 public class ChatPattern {
 	/**
 	 * Inventory slots the pattern corresponds to
@@ -13,9 +16,21 @@ public class ChatPattern {
 		BOOTS,
 	}
 
+	/**
+	 * The type the pattern has
+	 */
 	public final Type type;
+
+	/**
+	 * The regex pattern that this pattern resolves
+	 */
 	public final String pattern;
 
+	/**
+	 * Creates a new ChatPattern
+	 * @param type {@link ChatPattern.Type} the type of the chatpattern
+	 * @param pattern String of the regex pattern
+	 */
 	public ChatPattern (Type type, String pattern) {
 		this.type = type;
 		this.pattern = pattern;
